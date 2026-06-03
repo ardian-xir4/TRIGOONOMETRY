52dd052ecf9b56b79866cfa201da506d22b0e83a
ardian-xir4 <h1m4j0@gmail.com>
2026-06-03 09:54:56 +0700

    namas

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -2,11 +2,11 @@ import { IsEmail, IsString, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
-  @ApiProperty({ example: 'mechanic@gearbox.com' })
+  @ApiProperty({ example: 'majo@example.com' })
   @IsEmail()
   email: string;
 
-  @ApiProperty({ example: 'supersecret123', minLength: 6 })
+  @ApiProperty({ example: 'gataw123', minLength: 6 })
   @IsString()
   @MinLength(6)
   password: string;
